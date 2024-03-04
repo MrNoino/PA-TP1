e83a87c2691b9c955f5a1aa171a0fb964333d5cf
MrNoino <nuno.miguelsl25@gmail.com>
2024-03-04 19:28:38 +0000

    Added the get queries for users, managers, reviewers and authors

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -1,3 +1,100 @@
+### SELECTS
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`get_user_by_email` ;
+DELIMITER $$
+CREATE PROCEDURE get_user_by_email(IN a_email VARCHAR(256)) 
+BEGIN
+    SELECT users.id as "id"
+	FROM users
+    INNER JOIN managers
+    ON managers.user_id = users.id
+    WHERE users.id = a_id;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`get_manager` ;
+DELIMITER $$
+CREATE PROCEDURE get_manager(IN a_id BIGINT) 
+BEGIN
+    SELECT users.id as "id", 
+			users.name as "name", 
+            users.username as "username", 
+            users.active as "active", 
+            users.deleted as "deleted", 
+            users.role_id as "roleId"
+	FROM users
+    INNER JOIN managers
+    ON managers.user_id = users.id
+    WHERE users.id = a_id;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`get_reviewer` ;
+DELIMITER $$
+CREATE PROCEDURE get_reviewer(IN a_id BIGINT) 
+BEGIN
+    SELECT users.id as "id", 
+			users.name "name", 
+            users.username as "username", 
+            users.active as "active", 
+            users.deleted as "deleted", 
+            users.role_id as "roleId", 
+            reviewers.nif as "nif", 
+            reviewers.phone as "phone", 
+            reviewers.address as "address", 
+            reviewers.graduation as "graduation", 
+            reviewers.specialization as "specialization"
+	FROM users
+    INNER JOIN reviewers
+    ON reviewers.user_id = users.id
+    WHERE users.id = a_id;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`get_author` ;
+DELIMITER $$
+CREATE PROCEDURE get_author(IN a_id BIGINT) 
+BEGIN
+    SELECT users.id as "id", 
+		users.name as "name", 
+        users.username as "username", 
+        users.active as "active", 
+        users.deleted as "deleted", 
+        users.role_id as "roleId", 
+        authors.nif as "nif", 
+        authors.phone as "phone", 
+        authors.address as "address", 
+        authors.activity_begin_date as "activityBeginDate", 
+        authors.literary_style_id as "literaryStyleId"
+	FROM users
+    INNER JOIN authors
+    ON authors.user_id = users.id
+    WHERE users.id = a_id;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`exists_email` ;
+DELIMITER $$
+CREATE PROCEDURE exists_email(IN a_email VARCHAR(256)) 
+BEGIN
+    SELECT COUNT(email) as "exists"
+	FROM users
+	WHERE email = a_email;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`exists_username` ;
+DELIMITER $$
+CREATE PROCEDURE exists_username(IN a_username VARCHAR(128)) 
+BEGIN
+    SELECT COUNT(username) as "exists"
+	FROM users
+	WHERE username = a_username;
+END $$
+DELIMITER ;
+
+### INSERTS
+
 DROP PROCEDURE IF EXISTS `PA_TP`.`insert_manager` ;
 DELIMITER $$
 CREATE PROCEDURE insert_manager(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_role_id INT)
@@ -5,6 +102,7 @@ BEGIN
 	INSERT INTO users (name, username, password, email, active, role_id)
     VALUES
     (a_name, a_username, a_password, a_email, 1, a_role_id);
+    
     INSERT INTO managers (user_id)
     VALUES
     (LAST_INSERT_ID());
@@ -18,6 +116,7 @@ BEGIN
 	INSERT INTO users (name, username, password, email, role_id)
     VALUES
     (a_name, a_username, a_password, a_email, a_role_id);
+    
     INSERT INTO reviewers (user_id, nif, phone, address, graduation, specialization)
     VALUES
     (LAST_INSERT_ID(), a_nif, a_phone, a_address, a_graduation, a_specialization);
@@ -31,28 +130,11 @@ BEGIN
 	INSERT INTO users (name, username, password, email, role_id)
     VALUES
     (a_name, a_username, a_password, a_email, a_role_id);
+    
     INSERT INTO authors (user_id, nif, phone, address, literary_style_id)
     VALUES
     (LAST_INSERT_ID(), a_nif, a_phone, a_address, a_literary_style_id);
 END $$
 DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `PA_TP`.`verify_email` ;
-DELIMITER $$
-CREATE PROCEDURE verify_email(IN a_email VARCHAR(256)) 
-BEGIN
-    SELECT COUNT(email)
-			FROM users
-            WHERE email = a_email;
-END $$
-DELIMITER ;
-
-DROP PROCEDURE IF EXISTS `PA_TP`.`verify_username` ;
-DELIMITER $$
-CREATE PROCEDURE verify_username(IN a_username VARCHAR(128)) 
-BEGIN
-    SELECT COUNT(username)
-			FROM users
-            WHERE username = a_username;
-END $$
-DELIMITER ;
+### UPDATES
---
+++ sql/views.sql
@@ -0,0 +1,50 @@
+DROP VIEW IF EXISTS `PA_TP`.`get_users` ;
+CREATE VIEW get_users AS
+SELECT id, name, username, active, deleted, role_id as "roleId"
+FROM users;
+
+DROP VIEW IF EXISTS `PA_TP`.`get_managers` ;
+CREATE VIEW get_managers AS
+SELECT users.id as "id", 
+		users.name as "name", 
+        users.username as "username", 
+        users.active as "active", 
+        users.deleted as "deleted", 
+        users.role_id as "roleId"
+FROM users
+INNER JOIN managers
+ON managers.user_id = users.id;
+
+DROP VIEW IF EXISTS `PA_TP`.`get_reviewers` ;
+CREATE VIEW get_reviewers AS
+SELECT users.id as "id", 
+		users.name "name", 
+        users.username as "username", 
+        users.active as "active", 
+        users.deleted as "deleted", 
+        users.role_id as "roleId", 
+        reviewers.nif as "nif", 
+        reviewers.phone as "phone", 
+        reviewers.address as "address", 
+        reviewers.graduation as "graduation", 
+        reviewers.specialization as "specialization"
+FROM users
+INNER JOIN reviewers
+ON reviewers.user_id = users.id;
+
+DROP VIEW IF EXISTS `PA_TP`.`get_authors` ;
+CREATE VIEW get_authors AS
+SELECT users.id as "id", 
+		users.name as "name", 
+        users.username as "username", 
+        users.active as "active", 
+        users.deleted as "deleted", 
+        users.role_id as "roleId", 
+        authors.nif as "nif", 
+        authors.phone as "phone", 
+        authors.address as "address", 
+        authors.activity_begin_date as "activityBeginDate", 
+        authors.literary_style_id as "literaryStyleId"
+FROM users
+INNER JOIN authors
+ON authors.user_id = users.id;
