cc49aadf83d723c1cfb1cf0f32035c0861678416
MrNoino <nuno.miguelsl25@gmail.com>
2024-03-06 17:06:43 +0000

    Added login, change deleted and change active procedure and refactored the insert and update on users

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -93,15 +93,33 @@ BEGIN
 END $$
 DELIMITER ;
 
+DROP PROCEDURE IF EXISTS `PA_TP`.`login` ;
+DELIMITER $$
+CREATE PROCEDURE login(IN a_username VARCHAR(128)) 
+BEGIN
+    SELECT password
+	FROM users
+	WHERE username = a_username and active = 1 and deleted = 0;
+END $$
+DELIMITER ;
+
 ### INSERTS
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`insert_user` ;
+DELIMITER $$
+CREATE PROCEDURE insert_user(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_active TINYINT, IN a_role_id INT)
+BEGIN
+	INSERT INTO users (name, username, password, email, active, role_id)
+    VALUES
+    (a_name, a_username, a_password, a_email, a_active, a_role_id);
+END $$
+DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `PA_TP`.`insert_manager` ;
 DELIMITER $$
 CREATE PROCEDURE insert_manager(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_role_id INT)
 BEGIN
-	INSERT INTO users (name, username, password, email, active, role_id)
-    VALUES
-    (a_name, a_username, a_password, a_email, 1, a_role_id);
+    CALL insert_user(a_name, a_username, a_password, a_email, 1, a_role_id);
     
     INSERT INTO managers (user_id)
     VALUES
@@ -113,9 +131,7 @@ DROP PROCEDURE IF EXISTS `PA_TP`.`insert_reviewer` ;
 DELIMITER $$
 CREATE PROCEDURE insert_reviewer(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_role_id INT, IN a_nif VARCHAR(9), IN a_phone VARCHAR(9), IN a_address VARCHAR(256), IN a_graduation VARCHAR(128), IN  a_specialization VARCHAR(128))
 BEGIN
-	INSERT INTO users (name, username, password, email, role_id)
-    VALUES
-    (a_name, a_username, a_password, a_email, a_role_id);
+    CALL insert_user(a_name, a_username, a_password, a_email, 0, a_role_id);
     
     INSERT INTO reviewers (user_id, nif, phone, address, graduation, specialization)
     VALUES
@@ -127,9 +143,7 @@ DROP PROCEDURE IF EXISTS `PA_TP`.`insert_author` ;
 DELIMITER $$
 CREATE PROCEDURE insert_author(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_role_id INT, IN a_nif VARCHAR(9), IN a_phone VARCHAR(9), IN a_address VARCHAR(256), IN  a_literary_style_id INT)
 BEGIN
-	INSERT INTO users (name, username, password, email, role_id)
-    VALUES
-    (a_name, a_username, a_password, a_email, a_role_id);
+	CALL insert_user(a_name, a_username, a_password, a_email, 0, a_role_id);
     
     INSERT INTO authors (user_id, nif, phone, address, literary_style_id)
     VALUES
@@ -138,3 +152,65 @@ END $$
 DELIMITER ;
 
 ### UPDATES
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`update_user` ;
+DELIMITER $$
+CREATE PROCEDURE update_user(IN a_id BIGINT, IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_role_id INT)
+BEGIN
+	UPDATE users 
+    SET 
+    name = a_name,
+    username = a_username,
+    password = a_password, 
+    email = a_email,
+    active = a_active,
+    role_id = a_role_id
+    WHERE id = a_id;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`update_reviewer` ;
+DELIMITER $$
+CREATE PROCEDURE update_reviewer(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_active TINYINT, IN a_role_id INT, IN a_nif VARCHAR(9), IN a_phone VARCHAR(9), IN a_address VARCHAR(256), IN a_graduation VARCHAR(128), IN  a_specialization VARCHAR(128))
+BEGIN
+    CALL update_user(a_name, a_username, a_password, a_email, a_active, a_role_id);
+    
+    INSERT INTO reviewers (user_id, nif, phone, address, graduation, specialization)
+    VALUES
+    (LAST_INSERT_ID(), a_nif, a_phone, a_address, a_graduation, a_specialization);
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`update_author` ;
+DELIMITER $$
+CREATE PROCEDURE update_author(IN a_name VARCHAR(128), IN a_username VARCHAR(100), IN a_password VARCHAR(256), IN a_email VARCHAR(256), IN a_active TINYINT, IN a_role_id INT, IN a_nif VARCHAR(9), IN a_phone VARCHAR(9), IN a_address VARCHAR(256), IN  a_literary_style_id INT)
+BEGIN
+    CALL update_user(a_name, a_username, a_password, a_email, a_active, a_role_id);
+    
+    INSERT INTO authors (user_id, nif, phone, address, literary_style_id)
+    VALUES
+    (LAST_INSERT_ID(), a_nif, a_phone, a_address, a_literary_style_id);
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`change_deleted_user` ;
+DELIMITER $$
+CREATE PROCEDURE change_deleted_user(IN a_id BIGINT, IN a_deleted TINYINT)
+BEGIN
+    UPDATE users
+    SET 
+    deleted = a_deleted
+    WHERE id = a_id;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `PA_TP`.`change_active_user` ;
+DELIMITER $$
+CREATE PROCEDURE change_active_user(IN a_id BIGINT, IN a_active TINYINT)
+BEGIN
+    UPDATE users
+    SET 
+    active = a_active
+    WHERE id = a_id;
+END $$
+DELIMITER ;
