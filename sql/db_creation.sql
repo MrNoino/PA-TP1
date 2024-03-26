6e0a9b25aa4484c109b34deeaa535c9ef6fef3b4
lighttigerxiv <lighttigerxiv@gmail.com>
2024-03-26 11:41:03 +0000

    rename to completion_date

--- sql/db_creation.sql
+++ sql/db_creation.sql
@@ -199,7 +199,7 @@ CREATE TABLE IF NOT EXISTS `PA_TP`.`Reviews` (
   `serial_number` VARCHAR(128) NOT NULL,
   `submission_date` DATE NOT NULL DEFAULT (CURRENT_DATE),
   `approval_date` DATE NULL,
-  `completation_date` DATE NULL,
+  `completion_date` DATE NULL,
   `elapsed_time` INT NULL,
   `observations` VARCHAR(512) NULL,
   `cost` FLOAT NULL,
